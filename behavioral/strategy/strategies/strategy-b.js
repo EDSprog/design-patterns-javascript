73d684b051d77304d6a23e689ae53d6e3e033988
Edem Devlet <exter.edem.devlet@vwfs.io>
2024-04-14 00:11:36 +0200

    small fixes

--- behavioral/strategy/strategies/strategy-b.js
+++ behavioral/strategy/strategies/strategy-b.js
@@ -2,6 +2,6 @@ import { AbstractStrategy } from './abstract-strategy.js';
 
 export class StrategyB extends AbstractStrategy {
     execute(context) {
-        return `Execute ConcreteStrategyA with ${context.arg1} and ${context.arg2}`;
+        return `Execute ConcreteStrategyB with ${context.arg1} and ${context.arg2}`;
     }
 }
